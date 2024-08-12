2ff39203c5f86ad38e486c019a0127e2b95a068f
Laurian Gridinoc <laurian@gridinoc.name>
2024-08-12 15:23:35 +0300

    player via npm

--- timedtext-player/utils.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function interpolate(str: string, params: {
-    [key: string]: any;
-}): any;
-export declare function getTimeRange(element: HTMLElement): number[];
-export declare function getOffset(element: HTMLElement): number;
-//# sourceMappingURL=utils.d.ts.map
