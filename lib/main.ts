7dc7d49e76260fa9d3930e8bb78caf9bee4f83c1
Laurian Gridinoc <laurian@gridinoc.name>
2024-11-20 02:30:06 +0900

    exports, etc

--- lib/main.ts
+++ lib/main.ts
@@ -1,6 +1,10 @@
-import RemixContext from "./RemixContext";
+import RemixContext, { Context } from "./RemixContext";
 import RemixSource from "./RemixSource";
 import RemixSources from "./RemixSources";
 import RemixDestination from "./RemixDestination";
+import { StaticRemix } from "./StaticRemix";
+import { ts2timeline } from "./utils";
+import type { Timeline } from "./interfaces";
+import { EMPTY_REMIX } from "./utils";
 
-export { RemixContext, RemixSource, RemixSources, RemixDestination };
+export { Context,RemixContext, RemixSource, RemixSources, RemixDestination, StaticRemix, ts2timeline, Timeline, EMPTY_REMIX };
